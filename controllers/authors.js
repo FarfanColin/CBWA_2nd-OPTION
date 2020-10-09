52c070fcad98830377b4340bfed64c9f0e60fb30
FarfanColin <cjfc12345@hotmail.com>
2020-10-09 20:46:49 +0100

    fix_authors

--- controllers/authors.js
+++ controllers/authors.js
@@ -2,15 +2,21 @@ const authors = require('../models/authors.js')();
 module.exports = () => {
 const getController = (req, res) => {
  res.setHeader('Content-Type', 'application/json');
- res.json(authors.get());
+ return res.json(authors.get());
  }
+ const getById = (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.json(authors.get(req.params.id));
+  }
 const postController = (req, res) => {
  const name = req.body.name;
- authors.add(name);
+ const book = req.body.book;
+ authors.add(name, book);
  return res.end(`POST: ${name}`);
  }
 return {
  getController,
  postController,
+ getById
  }
 }
