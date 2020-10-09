a882ab0df380f550fe6e472ece125b7fe0264382
FarfanColin <cjfc12345@hotmail.com>
2020-10-09 01:11:11 +0100

    fix

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/authors', authorController.getController);
 // Add a author
 app.post('/authors', authorController.postController);
 
-app.get('/books/:id', booksController.getById);
+app.get('/authors/:id', booksController.getById);
 
 app.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
