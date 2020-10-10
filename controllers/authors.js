f730604c829668cb6f2cfb3a8b5bca0401495297
FarfanColin <cjfc12345@hotmail.com>
2020-10-10 23:35:39 +0100

    HopeLast

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,15 +1,15 @@
 const authors = require ( '../models/authors.js' )();
-module . exports = () => {
-  const getController = async ( req , res ) => {
-    res . json ( await authors . get ());
+module.exports = () => {
+  const getController = async ( req, res ) => {
+    res.json ( await authors.get ());
   }
-  const getById = async ( req , res ) => {
-    res . json ({ error: "byId not implemented yet" });
+  const getById = async ( req, res ) => {
+    res.json ({ error: "byId not implemented yet" });
   }
-  const postController = async ( req , res ) => {
-    const name = req . body . name ;
-    const result = await authors . add ( name );
-    res . json ( result );
+  const postController = async ( req, res ) => {
+    const name = req.body.name ;
+    const result = await authors.add ( name );
+    res.json ( result );
   }
   return {
     getController ,
--- models/books.js
+++ models/books.js
@@ -1,25 +1,25 @@
 const db = require ( '../db' )();
 const COLLECTION = "books" ;
-module . exports = () => {
+module.exports = () => {
   const get = async ( id = null ) => {
-    console . log ( ' inside books model' );
+    console.log ( ' inside books model' );
     if (! id ) {
-      const authors = await db . get ( COLLECTION );
+      const authors = await db.get ( COLLECTION );
       return authors ;
     }
     return { error: "byId not implemented yet" }
   }
-  const add = async ( name , author ) => {
-    const bookCount = await db . count ( COLLECTION );
-    const results = await db . add ( COLLECTION , {
+  const add = async ( name, author ) => {
+    const bookCount = await db.count ( COLLECTION );
+    const results = await db.add ( COLLECTION , {
       id: bookCount + 1 ,
-      name: name ,
+      name: name,
       author: author
     });
-    return results . result ;
+    return results.result ;
   }
   return {
-    get ,
+    get,
     add
   }
 };
