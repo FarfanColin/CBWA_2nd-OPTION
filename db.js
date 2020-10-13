c26a36ed86a91ceda7f0e92a31428cf8bd4245b7
FarfanColin <cjfc12345@hotmail.com>
2020-10-13 13:35:07 +0100

    upd

--- db.js
+++ db.js
@@ -40,31 +40,17 @@ module.exports = () => {
     });
   };
 
-  db.authors.remove({});
-  db.books.remove({});
 
-  var authors = [
-  { id: 1 , name: 'William Gibson' },
-  { id: 2 , name: 'Neil Stephenson' }
-  ];
 
-  var books = [
-  { id: 1 , name: 'Snow Crash' , author: 2 },
-  { id: 2 , name: 'Cryptonomicon' , author: 2 },
-  { id: 3 , name: 'Neuromancer' , author: 1 }
-  ];
-  db.authors.insertMany(authors);
-  db.books.insertMany(books);
-
-  // const authors = [
-  //  { id: 1, name: 'William Gibson' },
-  //  { id: 2, name: 'Neil Stephenson' }
-  //  ];
-  // const books = [
-  //  { id: 1, name: 'Snow Crash', author: 2},
-  //  { id: 2, name: 'Cryptonomicon', author: 2},
-  //  { id: 3, name: 'Neuromancer', author: 1}
-  //  ];
+  const authors = [
+   { id: 1, name: 'William Gibson' },
+   { id: 2, name: 'Neil Stephenson' }
+   ];
+  const books = [
+   { id: 1, name: 'Snow Crash', author: 2},
+   { id: 2, name: 'Cryptonomicon', author: 2},
+   { id: 3, name: 'Neuromancer', author: 1}
+   ];
 
   return {
     books,
