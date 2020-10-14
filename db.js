f20b135a9849a87ead478ff106b534f3e6f0e854
FarfanColin <cjfc12345@hotmail.com>
2020-10-14 19:01:25 +0100

    Update

--- db.js
+++ db.js
@@ -2,12 +2,14 @@ const uri = process.env.MONGO_URI;
 const MongoClient = require('mongodb').MongoClient;
 const DB_NAME = "book-store";
 const MONGO_OPTIONS = { useUnifiedTopology: true, useNewUrlParser: true };
+
 module.exports = () => {
   const count = (collectionName) => {
     return new Promise((resolve, reject) => {
       MongoClient.connect(uri, MONGO_OPTIONS, (err, client) => {
         const db = client.db(DB_NAME);
         const collection = db.collection(collectionName);
+
         collection.countDocuments({}, (err, docs) => {
           resolve(docs);
           client.close();
@@ -15,11 +17,13 @@ module.exports = () => {
       });
     });
   };
+
   const get = (collectionName, query = {})=> {
     return new Promise((resolve, reject) => {
       MongoClient.connect(uri, MONGO_OPTIONS, (err, client) => {
         const db = client.db(DB_NAME);
         const collection = db.collection(collectionName);
+
         collection.find(query).toArray((err, docs) => {
           resolve(docs);
           client.close();
@@ -27,11 +31,13 @@ module.exports = () => {
       });
     });
   };
+
   const add = (collectionName, item) => {
     return new Promise((resolve, reject) => {
       MongoClient.connect(uri, MONGO_OPTIONS, (err, client) => {
         const db = client.db(DB_NAME);
         const collection = db.collection(collectionName);
+
         collection.insertOne(item, (err, result) => {
           resolve(result);
           client.close();
@@ -40,24 +46,42 @@ module.exports = () => {
     });
   };
 
+  const aggregate = (collectionName, pipeline = []) => {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(uri, MONGO_OPTIONS, (err, client) => {
+        const db = client.db(DB_NAME);
+        const collection = db.collection(collectionName);
+
+        collection.aggregate(pipeline).toArray((err, docs) => {
+          if (err) {
+            console.log(" ---aggregate ERROR ---");
+            console.log(err);
+          }
+          resolve(docs);
+          client.close();
+        });
+      });
+    });
+  };
 
 
-  const authors = [
-   { id: 1, name: 'William Gibson' },
-   { id: 2, name: 'Neil Stephenson' }
-   ];
-  const books = [
-   { id: 1, name: 'Snow Crash', author: 2},
-   { id: 2, name: 'Cryptonomicon', author: 2},
-   { id: 3, name: 'Neuromancer', author: 1}
-   ];
+  // const authors = [
+  //   { id: 1, name: 'William Gibson' },
+  //   { id: 2, name: 'Neil Stephenson' }
+  // ];
+  // const books = [
+  //   { id: 1, name: 'Snow Crash', author: 2},
+  //   { id: 2, name: 'Cryptonomicon', author: 2},
+  //   { id: 3, name: 'Neuromancer', author: 1}
+  // ];
 
   return {
-    books,
-    authors,
+    // books,
+    // authors,
     count,
     get,
     add,
+    aggregate,
   };
 };
 
