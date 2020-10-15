2af92337d4a32a16732a1b483e2c11fac7a470ff
FarfanColin <cjfc12345@hotmail.com>
2020-10-15 17:40:54 +0100

    update

--- index.js
+++ index.js
@@ -1,53 +1,65 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const hostname = "0.0.0.0";
 const port = process.env.PORT || 3000;
+
 const authorController = require("./controllers/authors")();
 const booksController = require("./controllers/books")();
+
 const users = require("./models/users")();
+
 const app = (module.exports = express());
+
+app.use('/',express.static('static'));
+
 // logging
 app.use((req, res, next) => {
-// Display log for requests
-console.log("[%s] %s -- %s", new Date(), req.method, req.url);
-next();
+  // Display log for requests
+  console.log("[%s] %s -- %s", new Date(), req.method, req.url);
+  next();
 });
+
 app.use(async (req, res, next) => {
-const FailedAuthMessage = {
- // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
- error: "Failed Authentication",
- message: "Go away!",
- code: "xxx", // Some useful error code
- };
-const suppliedKey = req.headers["x-api-key"];
-const clientIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-// Check Pre-shared key
-if (!suppliedKey) {
- console.log(
- " [%s] FAILED AUTHENTICATION -- %s, No Key Supplied",
- new Date(),
- clientIp
- );
- FailedAuthMessage.code = "01";
- return res.status(401).json(FailedAuthMessage);
- }
-const user = await users.getByKey(suppliedKey);
-if (!user) {
- console.log(
- " [%s] FAILED AUTHENTICATION -- %s, BAD Key Supplied",
- new Date(),
- clientIp
- );
- FailedAuthMessage.code = "02";
- return res.status(401).json(FailedAuthMessage);
- }
-next();
+  const FailedAuthMessage = {
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+    error: "Failed Authentication",
+    message: "Go away!",
+    code: "xxx", // Some useful error code
+  };
+
+  const suppliedKey = req.headers["x-api-key"];
+  const clientIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+  // Check Pre-shared key
+  if (!suppliedKey) {
+    console.log(
+      " [%s] FAILED AUTHENTICATION -- %s, No Key Supplied",
+      new Date(),
+      clientIp
+    );
+    FailedAuthMessage.code = "01";
+    return res.status(401).json(FailedAuthMessage);
+  }
+
+  const user = await users.getByKey(suppliedKey);
+  if (!user) {
+    console.log(
+      " [%s] FAILED AUTHENTICATION -- %s, BAD Key Supplied",
+      new Date(),
+      clientIp
+    );
+    FailedAuthMessage.code = "02";
+    return res.status(401).json(FailedAuthMessage);
+  }
+  next();
 });
 app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-res.json({
- hello: "world",
- });
+  res.json({
+    hello: "world",
+  });
 });
 // Get all books
 app.get("/books", booksController.getController);
@@ -65,13 +77,14 @@ app.get("/authors/populated", authorController.populatedController);
 app.post("/authors", authorController.postController);
 // An Author
 app.get("/authors/:id", authorController.getById);
+
 app.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`);
-});
-// 404
-app.use((req, res) => {
-res.status(404).json({
- error: 404,
- message: "Route not found",
- });
-});
+  console.log(`Server running at http://${hostname}:${port}/`);
+  });
+  // 404
+  app.use((req, res) => {
+    res.status(404).json({
+      error: 404,
+      message: "Route not found",
+    });
+  });
